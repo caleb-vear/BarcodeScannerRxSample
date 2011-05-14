608125f12f982e51dd97ba0f8b2c6d986f5ecd32
Caleb Vear <caleb.vear@gmail.com>
2011-05-14 12:29:53 +1000

    Moved barcode reading operator to its own class

--- Program.cs
+++ Program.cs
@@ -39,30 +39,7 @@ namespace BarcodeScannerRx
 
                 userInput.OnNext(key.KeyChar);
             }
-        }
-
-        static IObservable<string> ToBarcodeReadings(this IObservable<char> input)
-        {
-            input = input
-                .Publish() // My test uses a cold observable so multiple subscriptions get the full sequence, publish fixes that.
-                .RefCount(); // Ref cound means that once we have no more subscribers kill the subscription to the base input.
-
-            var timeOut = input.Select(_ => Observable.Interval(TimeSpan.FromSeconds(5)))
-                .Switch()
-                .Do(_ => Console.WriteLine("\nInput Timed Out"));
-
-            var sequenceStarts = input.Where(c => c == '^');
-            var sequenceEnds = input.Where(c => c == '$');
-
-            var readings = from start in sequenceStarts
-                            select input
-                                .TakeUntil(sequenceEnds)
-                                .Aggregate("", (c1, c2) => c1 + c2)
-                                .TakeUntil(timeOut); // I don't use TimeOut because I don't want the TimeoutException
-
-            // Switch turns an IObservable<IObservable<T>> into IObservable<T> always taking from the last IObservable<T> that was pushed through.
-            return readings.Switch();
-        }
+        }        
 
         static void WriteOutput(string output)
         {
